2b119b3a4c13ff1706ea2ae3b40242dccd2cc884
Jian Ruan <kien.ngynh@gmail.com>
2026-03-10 21:42:31 +0700

    feat: Switch `xdg.configFile` and `home.file` sources to use `mkOutOfStoreSymlink` for instant hot-reloading.

--- nixos/home.nix
+++ nixos/home.nix
@@ -13,65 +13,69 @@
   # Kitty: Ctrl+Shift+F5 to reload
   # Fish: instant on new shell
   # Waybar: auto-reloads by default
+  # Helper to symlink directly from the cloned repo instead of copying to Nix store
+  # This enables instant hot-reloading for apps that support it
+  rawConfigDir = "/etc/nixos/home/.config";
+
   xdg.configFile = {
-    "hypr".source = ../home/.config/hypr;
-    "kitty".source = ../home/.config/kitty;
-    "waybar".source = ../home/.config/waybar;
-    "fish".source = ../home/.config/fish;
-    "rofi".source = ../home/.config/rofi;
-    "dunst".source = ../home/.config/dunst;
-    "avizo".source = ../home/.config/avizo;
-    "wlogout".source = ../home/.config/wlogout;
-    "starship.toml".source = ../home/.config/starship.toml;
-    "btop".source = ../home/.config/btop;
-    "bottom".source = ../home/.config/bottom;
-    "bat".source = ../home/.config/bat;
-    "cava".source = ../home/.config/cava;
-    "fastfetch".source = ../home/.config/fastfetch;
-    "helix".source = ../home/.config/helix;
-    "yazi".source = ../home/.config/yazi;
-    "zathura".source = ../home/.config/zathura;
-    "mpv".source = ../home/.config/mpv;
-    "htop".source = ../home/.config/htop;
-    "lazygit".source = ../home/.config/lazygit;
-    "delta".source = ../home/.config/delta;
-    "swappy".source = ../home/.config/swappy;
-    "pypr".source = ../home/.config/pypr;
-    "qutebrowser".source = ../home/.config/qutebrowser;
-    "wezterm".source = ../home/.config/wezterm;
-    "zellij".source = ../home/.config/zellij;
-    "nushell".source = ../home/.config/nushell;
-    "aichat".source = ../home/.config/aichat;
-    "opencode".source = ../home/.config/opencode;
-    "posting".source = ../home/.config/posting;
-    "bacon".source = ../home/.config/bacon;
-    "tealdeer".source = ../home/.config/tealdeer;
+    "hypr".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/hypr";
+    "kitty".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/kitty";
+    "waybar".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/waybar";
+    "fish".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/fish";
+    "rofi".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/rofi";
+    "dunst".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/dunst";
+    "avizo".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/avizo";
+    "wlogout".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/wlogout";
+    "starship.toml".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/starship.toml";
+    "btop".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/btop";
+    "bottom".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/bottom";
+    "bat".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/bat";
+    "cava".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/cava";
+    "fastfetch".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/fastfetch";
+    "helix".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/helix";
+    "yazi".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/yazi";
+    "zathura".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/zathura";
+    "mpv".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/mpv";
+    "htop".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/htop";
+    "lazygit".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/lazygit";
+    "delta".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/delta";
+    "swappy".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/swappy";
+    "pypr".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/pypr";
+    "qutebrowser".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/qutebrowser";
+    "wezterm".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/wezterm";
+    "zellij".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/zellij";
+    "nushell".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/nushell";
+    "aichat".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/aichat";
+    "opencode".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/opencode";
+    "posting".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/posting";
+    "bacon".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/bacon";
+    "tealdeer".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/tealdeer";
 
     # GTK / Theming
-    "gtk-3.0".source = ../home/.config/gtk-3.0;
-    "gtk-4.0".source = ../home/.config/gtk-4.0;
-    "Kvantum".source = ../home/.config/Kvantum;
-    "xfce4".source = ../home/.config/xfce4;
-    "xsettingsd".source = ../home/.config/xsettingsd;
+    "gtk-3.0".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/gtk-3.0";
+    "gtk-4.0".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/gtk-4.0";
+    "Kvantum".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/Kvantum";
+    "xfce4".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/xfce4";
+    "xsettingsd".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/xsettingsd";
 
     # MIME types
-    "mimeapps.list".source = ../home/.config/mimeapps.list;
-    "user-dirs.dirs".source = ../home/.config/user-dirs.dirs;
-    "user-dirs.locale".source = ../home/.config/user-dirs.locale;
+    "mimeapps.list".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/mimeapps.list";
+    "user-dirs.dirs".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/user-dirs.dirs";
+    "user-dirs.locale".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/user-dirs.locale";
 
     # Misc config files
-    "topgrade.toml".source = ../home/.config/topgrade.toml;
-    "pavucontrol.ini".source = ../home/.config/pavucontrol.ini;
-    "nixpkgs".source = ../home/.config/nixpkgs;
+    "topgrade.toml".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/topgrade.toml";
+    "pavucontrol.ini".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/pavucontrol.ini";
+    "nixpkgs".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/nixpkgs";
 
     # Environment files
-    ".env.searxng".source = ../home/.config/.env.searxng;
+    ".env.searxng".source = config.lib.file.mkOutOfStoreSymlink "${rawConfigDir}/.env.searxng";
   };
 
   # Symlink home-level dotfiles
   home.file = {
-    ".gitconfig".source = ../home/.gitconfig;
-    ".gtkrc-2.0".source = ../home/.gtkrc-2.0;
-    ".npmrc".source = ../home/.npmrc;
+    ".gitconfig".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/home/.gitconfig";
+    ".gtkrc-2.0".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/home/.gtkrc-2.0";
+    ".npmrc".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/home/.npmrc";
   };
 }
